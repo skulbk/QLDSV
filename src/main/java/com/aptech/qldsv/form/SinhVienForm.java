db0b4900e7e27b0577f00ffe22d528b2d4a09021
toan.domanh <toan.domanh@vndirect.com.vn>
2020-08-11 17:34:40 +0700

    form sinh vien

---
+++ src/main/java/com/aptech/qldsv/form/SinhVienForm.java
@@ -0,0 +1,25 @@
+package com.aptech.qldsv.form;
+
+import javax.swing.*;
+
+public class SinhVienForm {
+    private JTextField StudentIdTxt;
+    private JTextField studentNameTxt;
+    private JLabel studentNameLabel;
+    private JLabel StudentIdLabel;
+    private JTextField birthdayTxt;
+    private JLabel birthdayLabel;
+    private JRadioButton namRdo;
+    private JRadioButton nuRdo;
+    private JLabel genderLabel;
+    private JTextArea addressTxt;
+    private JLabel addressLabel;
+    private JButton sửaButton;
+    private JButton resetButton;
+    private JButton thêmButton;
+    private JButton xóaButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
